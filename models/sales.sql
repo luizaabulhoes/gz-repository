3c855e23f491f4c098c7f564b2dda727d63221d1
luizaabulhoes@gmail.com <luizaabulhoes@gmail.com>
2023-08-02 13:43:03 +0000

    stage sales

--- models/sales.sql
+++ models/sales.sql
@@ -1,8 +1,6 @@
-{{ config(schema='transaction') }}
-
 WITH 
 
-  sales AS (SELECT * FROM `gz_raw_data.raw_gz_sales`)
+  sales AS (SELECT * FROM `original-frame-392409`.`gz_dev_1`.`stg_sales` )
 
   ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
 
@@ -20,7 +18,7 @@ SELECT
   ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
 ,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
-    ,{{margin('s.revenue','s.quantity*CAST(p.purchSE_PRICE AS FLOAT64)')}} AS margin
-    ,{{ margin_percent('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS margin_percent
+    ,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS margin 
+ ,ROUND( SAFE_DIVIDE( (s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64)) , s.revenue ) , 2) AS margin_percent
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id 
