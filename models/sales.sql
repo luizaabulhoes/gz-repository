aff0a1a8df56f7c9374726d2f5f23326375f98d3
luizaabulhoes@gmail.com <luizaabulhoes@gmail.com>
2023-08-02 14:39:55 +0000

    swas

--- models/sales.sql
+++ models/sales.sql
@@ -2,7 +2,7 @@ WITH
 
   sales AS (SELECT * FROM `original-frame-392409`.`gz_dev_1`.`stg_sales` )
 
-  ,product AS (SELECT * FROM 'stg_product')
+  ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
 
 SELECT
   s.date_date
@@ -15,10 +15,10 @@ SELECT
   -- revenue --
   ,s.turnover
   -- cost --
-  ,CAST(p.purchase_price AS FLOAT64) AS purchase_price
-,ROUND(s.qty*CAST(purchase_price AS FLOAT64),2) AS purchase_cost
+  ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
+,ROUND(s.qty*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
-    ,s.turnover - s.qty*CAST(p.purchase_price AS FLOAT64) AS margin 
+    ,s.turnover - s.qty*CAST(p.purchSE_PRICE AS FLOAT64) AS margin 
  ,ROUND( SAFE_DIVIDE( (s.turnover - s.qty*CAST(p.purchSE_PRICE AS FLOAT64)) , s.turnover ) , 2) AS margin_percent
 FROM sales s
 INNER JOIN product p ON s.products_id = p.products_id 
